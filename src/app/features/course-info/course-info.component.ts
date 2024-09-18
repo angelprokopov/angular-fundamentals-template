e84068b478faa0a8810c440c4cd9cd7f812eeb88
angelprokopov <81374123+angelprokopov@users.noreply.github.com>
2024-09-18 19:07:34 +0300

    Initial

--- src/app/features/course-info/course-info.component.ts
+++ src/app/features/course-info/course-info.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
-import Course from '@app/core/interfaces/course';
+import { Component, Input } from "@angular/core";
+import Course from "@app/core/interfaces/course";
 
 @Component({
-    selector: 'app-course-info',
-    templateUrl: './course-info.component.html',
-    styleUrls: ['./course-info.component.scss'],
+  selector: "app-course-info",
+  templateUrl: "./course-info.component.html",
+  styleUrls: ["./course-info.component.scss"],
 })
 export class CourseInfoComponent {
-    @Input() course!: Course;
+  @Input() course!: Course;
 }
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,18 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input } from "@angular/core";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fas, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-    selector: 'app-button',
-    templateUrl: './button.component.html',
-    styleUrls: ['./button.component.scss'],
+  selector: "app-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.scss"],
 })
 export class ButtonComponent {
-    @Input() buttonText?: string | undefined;
-    @Input() iconName?: IconDefinition | undefined;
-    @Input() buttonStyle?: string | undefined;
+  @Input() buttonText?: string | undefined;
+  @Input() iconName?: IconDefinition | undefined;
+  @Input() buttonStyle?: string | undefined;
 
-    constructor(library: FaIconLibrary) {
-        library.addIconPacks(fas);
-    }
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
 }
