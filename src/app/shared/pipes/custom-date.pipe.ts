2981b4fc8d03007e33822730b11369b1d1824853
angelprokopov <81374123+angelprokopov@users.noreply.github.com>
2024-09-22 18:08:24 +0300

    Initial commit

--- src/app/shared/pipes/custom-date.pipe.ts
+++ src/app/shared/pipes/custom-date.pipe.ts
@@ -1,8 +1,16 @@
-import { Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-    name: 'customDate'
+    name: "customDate",
 })
-export class CustomDatePipe {
-    // Add your code here
+export class CustomDatePipe implements PipeTransform {
+    transform(value: Date | string): string {
+        const date = new Date(value);
+
+        const day = String(date.getDate()).padStart(2, "0");
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const year = date.getFullYear();
+
+        return `${day}.${month}.${year}`;
+    }
 }
