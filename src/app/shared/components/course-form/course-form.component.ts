22cc839e02cb8a44de6e2bb6f649034eaa2d7d45
angelprokopov <81374123+angelprokopov@users.noreply.github.com>
2024-09-28 18:24:43 +0300

    Initial commit

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators, FormControl } from '@angular/forms';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { v4 as uuidv4 } from 'uuid';
@@ -8,9 +8,17 @@ import { v4 as uuidv4 } from 'uuid';
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseFormComponent {
   courseForm!: FormGroup;
+  formFields = {
+    title: 'title',
+    description: 'description',
+    duration: 'duration',
+    newAuthor: 'newAuthor',
+    authorName: 'name',
+  }
 
   constructor(private fb: FormBuilder, private library: FaIconLibrary) {
     library.addIconPacks(fas);
@@ -19,16 +27,17 @@ export class CourseFormComponent {
 
   buildForm(): void {
     this.courseForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(2)]],
-      description: ['', [Validators.required, Validators.minLength(2)]],
+      [this.formFields.title]: ['', [Validators.required, Validators.minLength(2)]],
+      [this.formFields.description]: ['', [Validators.required, Validators.minLength(2)]],
       authors: this.fb.array([]),
       courseAuthors: this.fb.array([]),
-      author: this.fb.group({
-        name: ['', [Validators.minLength(2), Validators.pattern('^[a-zA-Z0-9 ]+$')]],
+      [this.formFields.newAuthor]: this.fb.group({
+        [this.formFields.authorName]: ['', [Validators.minLength(2), Validators.pattern('^[a-zA-Z0-9 ]+$')]], // name control
       }),
-      duration: ['', [Validators.required, Validators.min(0)]],
+      [this.formFields.duration]: ['', [Validators.required, Validators.min(0)]],
     });
   }
+
 
   get authors(): FormArray {
     return this.courseForm.get('authors') as FormArray;
@@ -38,36 +47,67 @@ export class CourseFormComponent {
     return this.courseForm.get('courseAuthors') as FormArray;
   }
 
+  get titleControl(): FormControl{
+    return this.courseForm.get(this.formFields.title)! as FormControl;
+  }
+
+  get descriptionControl(): FormControl{
+    return this.courseForm.get(this.formFields.description)! as FormControl;
+  }
+
+  get durationControl(): FormControl{
+    return this.courseForm.get(this.formFields.duration)! as FormControl;
+  }
+
+  get newAuthorGroup(): FormGroup{
+    return this.courseForm.get(this.formFields.newAuthor)! as FormGroup;
+  }
+
+  get newAuthorNameControl(): FormControl{
+    return this.courseForm.get(this.formFields.newAuthor)?.get(this.formFields.authorName)! as FormControl;
+  }
+
   addAuthor(): void {
-    const nestedGroup = this.courseForm.get('author') as FormGroup;
-    const newAuthorControl = this.courseForm.get('author')?.get('name');
-    const authorName = newAuthorControl?.value;
+    const authorName = this.newAuthorNameControl?.value;
     const authorId = uuidv4();
 
-    if (authorName && nestedGroup?.valid) {
-      this.courseAuthors.push(this.fb.group({
-        id: [authorId],
-        name: [authorName]
-      }));
+    if (authorName && this.newAuthorGroup?.valid) {
       this.authors.push(this.fb.group({
         id: [authorId],
         name: [authorName]
       }));
-      newAuthorControl?.setValue('');
+      this.newAuthorNameControl?.setValue('');
     }
-    nestedGroup.markAllAsTouched();
+  }
+
+  addAuthorToCourseAuthor(index: number): void {
+    this.courseAuthors.push(this.authors.at(index));
+    this.authors.removeAt(index);
   }
 
   removeAuthor(index: number): void {
-    console.log('author removed');
     this.authors.removeAt(index);
   }
 
   removeCourseAuthor(index: number): void {
-    console.log('course author removed');
+    this.authors.push(this.courseAuthors.at(index));
     this.courseAuthors.removeAt(index);
   }
 
+  clearAuthorsAndCourseAuthors(): void {
+    this.authors.clear();
+    this.courseAuthors.clear();
+  }
+
+  formReset(): void {
+    this.courseForm.reset();
+    this.clearAuthorsAndCourseAuthors();
+  }
+
+  trackByFn(index: any, item: any) {
+    return item.id;
+  }
+  
   onSubmit(): void {
     if (this.courseForm.valid) {
       console.log('Form Submitted', this.courseForm.value);
--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -6,9 +6,15 @@ import { Router } from '@angular/router';
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegistrationFormComponent {
   registrationForm!: FormGroup;
+  formFields= {
+    name: 'name',
+    email: 'email',
+    password: 'password'
+  }
 
   constructor(private router: Router) {
     this.buildForm();
@@ -16,26 +22,31 @@ export class RegistrationFormComponent {
 
   buildForm(): void{
     this.registrationForm = new FormGroup({
-      name: new FormControl('',[Validators.required,Validators.minLength(6)]),
-      email: new FormControl('',[Validators.required]),
-      password: new FormControl('',[Validators.required,Validators.minLength(6)]),
+      [this.formFields.name]: new FormControl('',[Validators.required,Validators.minLength(6)]),
+      [this.formFields.email]: new FormControl('',[Validators.required]),
+      [this.formFields.password]: new FormControl('',[Validators.required,Validators.minLength(6)]),
     });
+  }
+
+  get nameControl(): FormControl {
+    return this.registrationForm.get(this.formFields.name)! as FormControl;
+  }
+
+  get emailControl(): FormControl {
+    return this.registrationForm.get(this.formFields.email)! as FormControl;
+  }
+
+  get passwordControl(): FormControl {
+    return this.registrationForm.get(this.formFields.password)! as FormControl;
   }
 
   onSubmit(): void {
     console.log(this.registrationForm.valid);
     if (this.registrationForm.valid) {
       console.log(this.registrationForm.value);
-      //Database operation here
+      // Handle form submission logic
     } else {
       this.registrationForm.markAllAsTouched();
-    }
-  }
-
-  triggerSubmit() {
-    const submitButton = document.querySelector('button[type="submit"]') as HTMLButtonElement;
-    if (submitButton) {
-      submitButton.click();
     }
   }
 
